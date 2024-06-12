73707cc453d467f7618fd9a47993277273e5c2c8
yohanesdewantara <yohanesdewantara1@gmail.com>
2024-06-12 22:21:35 +0700

    first commit

--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,6 @@
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\APIController;
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+
 
 Route::get('/_daftar_parfum/v1/search', 'APIController@searchparfum');
